490df00734a1524adcafb894c8d53c70bb5ab780
PJunhyuk <jgravity@naver.com>
2017-01-31 02:05:02 +0900

    Initialize train.lua

---
+++ train.lua
@@ -0,0 +1,7 @@
+os.execute('wget -c https://s3.amazonaws.com/torch7/data/cifar10torchsmall.zip')
+os.execute('unzip cifar10torchsmall.zip')
+trainset = torch.load('cifar10-train.t7')
+testset = torch.load('cifar10-test.t7')
+classes = {'airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
+print(trainset)
+print(#trainset.data)
