37ae59a2d91fdeca09e3391b6bc99be50c3b6213
PJunhyuk <jgravity@naver.com>
2017-01-31 03:17:57 +0900

    Implement data preprocessing

--- train.lua
+++ train.lua
@@ -1,5 +1,36 @@
+-- how to run: th train.lua
+
 trainset = torch.load('cifar10-train.t7')
 testset = torch.load('cifar10-test.t7')
 classes = {'airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
+
 print(trainset)
+--[[
+{
+    data : ByteTensor - size: 10000x3x32x32
+    label : ByteTensor - size: 10000
+}
+]]
+
 print(#trainset.data)
+--[[
+10000
+3
+32
+32
+[torch.LongStorage of size 4]
+]]
+
+setmetatable(trainset,
+    {__index = function(t, i)
+        return {t.data[i], t.label[i]}
+    end}
+);
+
+trainset.data = trainset.data:double()
+
+function trainset:size()
+    return self.data:size(1)
+end
+
+print(trainset:size())
